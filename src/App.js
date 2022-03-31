1089a760b0fffc794b4dcbd75bbef96f31f9a57b
HANSOL JEONG <losnah24@gmail.com>
2022-03-31 18:20:45 +0900

    Add: Login.js 페이지에서 컴포넌트로 변경, Header Layout, Login popup Layout, show Modal 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Route from './Route';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Route />
+    </div>
+  );
 }
 
 export default App;
--- src/Route.js
+++ src/Route.js
@@ -5,7 +5,7 @@ import Main from './pages/Main/Main';
 import List from './pages/List/List';
 import Detail from './pages/Detail/Detail';
 import ShopCart from './pages/ShopCart/ShopCart';
-import Login from './pages/Login/Login';
+// import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
 
 function Router() {
@@ -14,7 +14,7 @@ function Router() {
       <Routes>
         <Route path="/Main" element={<Main />} />
         <Route path="/List" element={<List />} />
-        <Route path="/Login" element={<Login />} />
+        {/* <Route path="/Login" element={<Login />} /> */}
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/Detail" element={<Detail />} />
         <Route path="/ShopCart" element={<ShopCart />} />
