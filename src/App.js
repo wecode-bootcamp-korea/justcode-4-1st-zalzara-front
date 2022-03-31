d0fc65ba654eacdac1a5f75d85a44f505a5e335b
최정환 <junghwan@jung.local>
2022-03-31 15:29:54 +0900

    ADD: header layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header';
 import Route from './Route';
 
 function App() {
   return (
     <div>
+      <Header />
       <Route />
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import './Header.scss';
+import {
+  AiOutlineMenu,
+  AiOutlineUser,
+  AiOutlineShopping,
+} from 'react-icons/ai';
 
 function Header() {
-  return <div>header</div>;
+  return (
+    <div className="Header">
+      <div className="homeName">
+        <AiOutlineMenu className="menuBar" />
+        Z&nbsp;A&nbsp;R&nbsp;A &nbsp;&nbsp;H&nbsp;O&nbsp;M&nbsp;E
+      </div>
+      <button className="searchBox">
+        검색 <div className="line" />
+      </button>
+      <div className="userInfo">
+        <button className="user">
+          <AiOutlineUser />
+          로그인
+        </button>
+        <button className="cart">
+          <AiOutlineShopping />0
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
