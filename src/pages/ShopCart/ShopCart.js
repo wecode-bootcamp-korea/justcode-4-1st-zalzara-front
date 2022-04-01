bbce31653ac80457c985a43b234b5f2bda0f6e69
최정환 <junghwan@jung.local>
2022-04-01 16:26:48 +0900

    ADD: restore

---
+++ src/pages/ShopCart/ShopCart.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import BasketCard from './components/BasketCard';
+import ShopLaterCard from './components/ShopLater';
+import { CartContext } from './Contexts';
+import './ShopCart.scss';
+
+export default function ShopCart() {
+  const [items, set_items] = useState([]);
+  const [card, setCard] = useState(<BasketCard />);
+  const [line, setLine] = useState('basket');
+
+  useEffect(() => {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    set_items([
+      {
+        url: 'https://image.shutterstock.com/z/stock-photo-scandinavian-wool-dot-carpet-rug-with-cotton-base-and-wool-dots-on-white-background-geometric-1749541253.jpg',
+        price: 412414,
+        name: '스프라이트 머시기',
+        id: `2132/1323/123`,
+        size: '더블/퀸 (230 x 250)',
+        later: 0,
+        count: 1,
+      },
+      {
+        url: 'https://image.shutterstock.com/z/stock-photo-scandinavian-wool-dot-carpet-rug-with-cotton-base-and-wool-dots-on-white-background-geometric-1749541253.jpg',
+        price: 123231,
+        name: '콜라',
+        id: `2132/1323/123`,
+        size: '더블/퀸 (230 x 250)',
+        later: 0,
+        count: 1,
+      },
+    ]);
+  }, []);
+
+  const basket_count = items => {
+    let count = 0;
+    items.map(i => {
+      if (i.later === 0) {
+        count += i.count;
+      }
+    });
+    return count;
+  };
+
+  const change_basket = event => {
+    event.preventDefault();
+    const { className } = event.target;
+    if (className === 'basket' || className === 'basket_out') {
+      setLine('basket');
+      return setCard(<BasketCard />);
+    } else {
+      setLine('shop-later');
+      return setCard(<ShopLaterCard />);
+    }
+  };
+
+  return (
+    <CartContext.Provider value={{ items }}>
+      <div className="ShopCart">
+        <header />
+        <nav className="nav_bar">
+          <div
+            className={line === 'basket' ? 'basket' : 'basket_out'}
+            onClick={change_basket}
+          >
+            장바구니 <span>({basket_count(items)})</span>
+          </div>{' '}
+          <div
+            className={line !== 'basket' ? 'shop_later' : 'shop_later_out'}
+            onClick={change_basket}
+          >
+            나중에 쇼핑하기 <span>(0)</span>
+          </div>
+        </nav>
+        {card}
+        <footer />
+      </div>
+    </CartContext.Provider>
+  );
+}
---
+++ src/pages/ShopCart/components/BasketCard.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState, useContext } from 'react';
+import './BasketCard.scss';
+import ProductCard from './ProductCard';
+import { CartContext, BasketContext } from '../Contexts';
+
+export default function BasketCard() {
+  let item = {
+    price: 23123,
+    count: 1,
+    later: false,
+  };
+  const { items } = useContext(CartContext);
+  console.log(items);
+  console.log(items.map(i => i.id));
+  const [is_none, set_is_none] = useState(true);
+  const [total_price, set_total_price] = useState(item.price);
+  const [count, set_count] = useState(item.count);
+  const [later, set_later] = useState(item.later);
+
+  const get_price = price => {
+    set_total_price(price);
+  };
+
+  const set_delete = later => {
+    set_later(later);
+  };
+
+  useEffect(() => {
+    items.length > 0 ? set_is_none(false) : set_is_none(true);
+  }, [item, count]);
+
+  return (
+    <div className="BasketCard">
+      {is_none ? (
+        <div className="none_description">
+          고객님의 장바구니가 비어있습니다.
+        </div>
+      ) : items.length && item.later === 1 ? (
+        set_is_none(true)
+      ) : (
+        <BasketContext.Provider value={{ items }}>
+          {items.map(product => (
+            <ProductCard item={item} product={product} get_price={get_price} />
+          ))}
+          <button className="clear_basket">장바구니 비우기</button>
+
+          <div className="order_box">
+            <div className="total_items">{item.count} 제품</div>
+            <div className="total_price_with_tax">
+              총 제품: 세금 포함 <span>{total_price}원</span>
+            </div>
+            <div className="total_price">
+              합계: <span>{total_price}원*</span>
+            </div>
+            <span className="promotion_alert">
+              프로모션 코드가 있으신가요? 나중에 결제 페이지에서 입력하십시오.
+            </span>
+            <button className="purchase_btn">주문하기</button>
+            <div className="free_deliver">무료 매장 배송 가능</div>
+          </div>
+        </BasketContext.Provider>
+      )}
+    </div>
+  );
+}
