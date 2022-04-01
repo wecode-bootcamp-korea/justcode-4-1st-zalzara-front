973ea6b1ebe47acd5f2199b1fe46d6473b134c59
haleyisfree <97721903+haleyisfree@users.noreply.github.com>
2022-04-01 15:55:28 +0900

    Update Router.js
    리스트 삭제
    메인 path 수정 '/'

--- src/Router.js
+++ src/Router.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Main from './pages/Main/Main';
-import List from './pages/List/List';
 import Detail from './pages/Detail/Detail';
 import ShopCart from './pages/ShopCart/ShopCart';
 
@@ -10,8 +9,7 @@ function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Main" element={<Main />} />
-        <Route path="/List" element={<List />} />
+        <Route path="/" element={<Main />} />
         <Route path="/Detail" element={<Detail />} />
         <Route path="/ShopCart" element={<ShopCart />} />
       </Routes>
