a2e24987f99736771472626971674955b63427f2
최정환 <junghwan@jung.local>
2022-04-07 11:48:38 +0900

    ADD: category page 추가

--- src/Router.js
+++ src/Router.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main/Main';
 import Detail from './pages/Detail/Detail';
 import ShopCart from './pages/ShopCart/ShopCart';
+import CategoryList from './pages/CategoryList/CategoryList';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/categories" element={<CategoryList />} />
         <Route path="/detail" element={<Detail />} />
         <Route path="/shop-cart" element={<ShopCart />} />
       </Routes>
